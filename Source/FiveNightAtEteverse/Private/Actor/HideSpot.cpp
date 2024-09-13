cdfae7ec5210e6e483791a0f1cc4e2156611434c
nichy98vr <nichy98@naver.com>
2024-09-13 17:31:22 +0900

    [feat] : HideSpot 개발시작

--- Source/FiveNightAtEteverse/Private/Actor/HideSpot.cpp
+++ Source/FiveNightAtEteverse/Private/Actor/HideSpot.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Actor/HideSpot.h"
+#include "Components/BoxComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
 
 // Sets default values
 AHideSpot::AHideSpot()
@@ -9,12 +11,59 @@ AHideSpot::AHideSpot()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	SetRootComponent(DefaultSceneRoot);
+	HideCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("HideCollisionBox"));
+	HideCollision->SetupAttachment(DefaultSceneRoot);
+
+	CabinetBox = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CabinetBoxStaticMesh"));
+	CabinetBox->SetupAttachment(HideCollision);
+	CabinetDoor = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CabinetDoorStaticMesh"));
+	CabinetDoor->SetupAttachment(CabinetBox);
+	CabinetOuterHandle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CabinetOuterHandleStaticMesh"));
+	CabinetOuterHandle->SetupAttachment(CabinetDoor);
+	CabinetInnerHandle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CabinetInnerHandleleStaticMesh"));
+	CabinetInnerHandle->SetupAttachment(CabinetDoor);
+
+	OuterHandleCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("OuterHandleCollisionBox"));
+	OuterHandleCollision->SetupAttachment(CabinetOuterHandle);
+	InnerHandleCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("InnerCollisionBox"));
+	InnerHandleCollision->SetupAttachment(CabinetInnerHandle);
+
+	OuterLeftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("OuterLeftHandSkeletalMesh"));
+	OuterLeftHand->SetupAttachment(CabinetOuterHandle);
+	OuterRightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("OuterRightHandSkeletalMesh"));
+	OuterRightHand->SetupAttachment(CabinetOuterHandle);
+	InnerLeftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("InnerLeftHandSkeletalMesh"));
+	InnerLeftHand->SetupAttachment(CabinetInnerHandle);
+	InnerRightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("InnerRightHandSkeletalMesh"));
+	InnerRightHand->SetupAttachment(CabinetInnerHandle);
+
+	Sign1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sign1StaticMesh"));
+	Sign1->SetupAttachment(CabinetDoor);
+	Sign2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sign2StaticMesh"));
+	Sign2->SetupAttachment(CabinetDoor);
+	Sign3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sign3StaticMesh"));
+	Sign3->SetupAttachment(CabinetDoor);
+
+	CabinetDoor->SetSimulatePhysics(true);
+
+	OuterLeftHand->SetVisibility(false);
+	OuterRightHand->SetVisibility(false);
+	InnerLeftHand->SetVisibility(false);
+	InnerRightHand->SetVisibility(false);
+
+	InnerHandleCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision); //HideCollision에 플레이어가 들어간다면 CollisionEnabled해주기
+
+	HideCollision->OnComponentBeginOverlap.AddDynamic(this, &AHideSpot::BeginOverlap);
+	HideCollision->OnComponentEndOverlap.AddDynamic(this, &AHideSpot::EndOverlap);
 }
 
 // Called when the game starts or when spawned
 void AHideSpot::BeginPlay()
 {
 	Super::BeginPlay();
+
 	
 }
 
@@ -23,5 +72,13 @@ void AHideSpot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AHideSpot::BeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void AHideSpot::EndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
--- Source/FiveNightAtEteverse/Public/Actor/HideSpot.h
+++ Source/FiveNightAtEteverse/Public/Actor/HideSpot.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Actor.h"
 #include "HideSpot.generated.h"
 
+class UBoxComponent;
+class UPrimitiveComponent;
+class UPhysicsConstraintComponent;
+
 UCLASS()
 class FIVENIGHTATETEVERSE_API AHideSpot : public AActor
 {
@@ -21,14 +25,61 @@ protected:
 
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
 	TObjectPtr<USceneComponent> DefaultSceneRoot;
-	/** Please add a variable description */
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UBoxComponent> HideCollision;
+
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
 	TObjectPtr<UStaticMeshComponent> CabinetBox;
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
 	TObjectPtr<UStaticMeshComponent> CabinetDoor;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> CabinetOuterHandle;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> CabinetInnerHandle;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UBoxComponent> OuterHandleCollision;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UBoxComponent> InnerHandleCollision;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<USkeletalMeshComponent> OuterLeftHand;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<USkeletalMeshComponent> OuterRightHand;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<USkeletalMeshComponent> InnerLeftHand;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<USkeletalMeshComponent> InnerRightHand;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> Sign1;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> Sign2;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	TObjectPtr<UStaticMeshComponent> Sign3;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Default")
+	bool IsClosed;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void BeginOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UFUNCTION()
+	void EndOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
 };
