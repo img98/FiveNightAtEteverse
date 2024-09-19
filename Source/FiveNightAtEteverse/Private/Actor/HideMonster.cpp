76271d44a0e52fde271f118b3dcca4e4e4d656ab
nichy98vr <nichy98@naver.com>
2024-09-19 17:19:03 +0900

    [feat] : HideMonster ABP와 로직 구현 + DoorMonster ABP수정

--- Source/FiveNightAtEteverse/Private/Actor/HideMonster.cpp
+++ Source/FiveNightAtEteverse/Private/Actor/HideMonster.cpp
@@ -9,12 +9,10 @@ bool AHideMonster::CheckPlayerStateSafe()
 	//플레이어가 캐비넷에 숨었는지 GameState를 통해 확인
 	if (GameState->GetIsPlayerHide())
 	{
-		UE_LOG(LogTemp, Log, TEXT("Hiding"));
 		return true;		
 	}
 	else
 	{
-		UE_LOG(LogTemp, Log, TEXT("Player is not Hiding"));
 		return false;
 	}
 }
--- Source/FiveNightAtEteverse/Public/Actor/HideMonster.h
+++ Source/FiveNightAtEteverse/Public/Actor/HideMonster.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Actor/MonsterCharacter.h"
-#include "Interface/MonsterInterface.h"
 #include "HideMonster.generated.h"
 
 /**
